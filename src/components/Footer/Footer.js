a01714d4aaf76727a76d66ae60ee76f015fe2446
eka-dyah <eka.dya1205@gmail.com>
2021-05-20 19:40:07 +0700

    edit Footer component

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,16 +1,37 @@
+import "./Footer.css";
+
 const Footer = () => (
-	<div>
-		Icons made by{" "}
-		<a
-			href="https://www.flaticon.com/authors/pixel-perfect"
-			title="Pixel perfect"
-		>
-			Pixel perfect
-		</a>{" "}
-		from{" "}
-		<a href="https://www.flaticon.com/" title="Flaticon">
-			www.flaticon.com
-		</a>
+	<div className="Footer">
+		<p>
+			Powered by{" "}
+			<a
+				target="_blank"
+				rel="noreferrer"
+				href="https://www.themoviedb.org"
+			>
+				The Movie Database (TMDb)
+			</a>
+		</p>
+		<p>
+			Icons made by{" "}
+			<a
+				target="_blank"
+				rel="noreferrer"
+				href="https://www.flaticon.com/authors/pixel-perfect"
+				title="Pixel perfect"
+			>
+				Pixel perfect
+			</a>{" "}
+			from{" "}
+			<a
+				target="_blank"
+				rel="noreferrer"
+				href="https://www.flaticon.com/"
+				title="Flaticon"
+			>
+				www.flaticon.com
+			</a>
+		</p>
 	</div>
 );
 
