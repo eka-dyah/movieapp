34ce77756ee4998dfbc1a9113c24a893fd4b2579
eka-dyah <eka.dya1205@gmail.com>
2021-05-27 17:34:52 +0700

    remove Layout component and using params to determine movie id

--- src/containers/MovieDetails.js
+++ src/containers/MovieDetails.js
@@ -1,12 +1,15 @@
 import { useEffect, useRef, useState } from "react";
+import { useParams } from "react-router-dom";
 import CreditList from "../components/CreditList/CreditList";
-import Layout from "../components/Layout/Layout";
 import MovieBanner from "../components/MovieBanner/MovieBanner";
 import ScrollableMovieList from "../components/ScrollableMovieList/ScrollableMovieList";
 import Spinner from "../components/Spinner/Spinner";
 import { getMoviePropById, getMoviesByCategory } from "../utils/fetchData";
 
 const MovieDetails = ({ movieId }) => {
+	const { id } = useParams();
+	let mId = movieId || id;
+
 	const [movies, setMovies] = useState({
 		movies: null,
 		error: null,
@@ -34,20 +37,21 @@ const MovieDetails = ({ movieId }) => {
 	});
 
 	const getMovies = () => {
-		setMovies({ ...movies, loading: true });
-		getMoviePropById(movieId, null, null, (error, data) => {
+		setMovies({ loading: true, error: null, movies: null });
+		getMoviePropById(mId, null, null, (error, data) => {
 			if (error) setMovies({ ...movies, loading: false, error: error });
-			else setMovies({
-				loading: false,
-				error: null,
-				movies: { ...data },
-			});
-		})
-	}
+			else
+				setMovies({
+					loading: false,
+					error: null,
+					movies: { ...data },
+				});
+		});
+	};
 
 	const getPerson = () => {
-		setPerson({ ...person, loading: true });
-		getMoviePropById(movieId, "credits", 1, (error, data) => {
+		setPerson({ loading: true, error: null, person: [] });
+		getMoviePropById(mId, "credits", 1, (error, data) => {
 			if (error) setPerson({ ...person, loading: false, error: error });
 			else {
 				let acting = data.cast.filter(
@@ -56,56 +60,62 @@ const MovieDetails = ({ movieId }) => {
 				acting = acting.length > 10 ? acting.slice(0, 10) : acting;
 				setPerson({ loading: false, error: null, person: [...acting] });
 			}
-		})
-	}
+		});
+	};
 
 	const getSimilar = () => {
-		setSimilar({ ...person, loading: true });
-		getMoviePropById(movieId, 'similar', 1, (error, data) => {
-			if (error) setSimilar({
-				loading: false,
-				error: error,
-				movies: [],
-			});
-			else setSimilar({
-				loading: false,
-				error: null,
-				movies: [...data.results],
-			});
-		})
-	}
+		setSimilar({ error: null, movies: [], loading: true });
+		getMoviePropById(mId, "similar", 1, (error, data) => {
+			if (error)
+				setSimilar({
+					loading: false,
+					error: error,
+					movies: [],
+				});
+			else
+				setSimilar({
+					loading: false,
+					error: null,
+					movies: [...data.results],
+				});
+		});
+	};
 
 	const getRecommendations = () => {
 		setRecommendations({ ...person, loading: true });
-		getMoviePropById(movieId, 'recommendations', 1, (error, data) => {
-			if (error) setRecommendations({
-				loading: false,
-				error: error,
-				movies: [],
-			});
-			else setRecommendations({
-				loading: false,
-				error: null,
-				movies: [...data.results],
-			});
-		})
-	}
+		getMoviePropById(mId, "recommendations", 1, (error, data) => {
+			if (error)
+				setRecommendations({
+					loading: false,
+					error: error,
+					movies: [],
+				});
+			else
+				setRecommendations({
+					loading: false,
+					error: null,
+					movies: [...data.results],
+				});
+		});
+	};
 
 	const getPopular = () => {
 		setPopular({ ...person, loading: true });
 		getMoviesByCategory("popular", 1, (error, data) => {
-			if (error) setPopular({
-				loading: false,
-				error: error,
-				movies: [],
-			});
-			else setPopular({
-				loading: false,
-				error: null,
-				movies: [...data.results],
-			});
-		})
-	}
+			if (error)
+				setPopular({
+					loading: false,
+					error: error,
+					movies: [],
+				});
+			else
+				setPopular({
+					loading: false,
+					error: null,
+					movies: [...data.results],
+				});
+		});
+	};
 
 	const getMoviesRef = useRef();
 	const getPersonRef = useRef();
@@ -120,31 +130,38 @@ const MovieDetails = ({ movieId }) => {
 	getPopularRef.current = getPopular;
 
 	useEffect(() => {
+		window.scrollTo(0, 0);
 		getMoviesRef.current();
 		getPersonRef.current();
 		getSimilarRef.current();
 		getRecommendationsRef.current();
-	}, []);
+	}, [mId]);
 
 	useEffect(() => {
 		if (movies.error) {
 			getPopularRef.current();
 		}
-	}, [movies])
+	}, [movies]);
 
 	let content;
 	if (movies.loading) {
 		content = <Spinner />;
 	} else if (movies.error) {
-		content = <p className="text-center text-error">{movies.error || "An error has occured."}</p>;
+		content = (
+			<p className="text-center text-error">
+				{movies.error || "An error has occured."}
+			</p>
+		);
 	} else if (movies.movies && Object.keys(movies.movies).length !== 0) {
 		content = (
 			<>
 				<MovieBanner movieDetails={movies.movies} />
-				<CreditList
-					type="companies"
-					info={movies.movies.production_companies}
-				/>
+				{movies.movies.production_companies.length !== 0 && (
+					<CreditList
+						type="companies"
+						info={movies.movies.production_companies}
+					/>
+				)}
 			</>
 		);
 	}
@@ -153,9 +170,9 @@ const MovieDetails = ({ movieId }) => {
 	if (person.person.length !== 0) {
 		personCredit = <CreditList type="acting" info={person.person} />;
 	} else personCredit = null;
-	
+
 	return (
-		<Layout>
+		<>
 			{content}
 			{personCredit}
 			<ScrollableMovieList category="similar" movies={similar.movies} />
@@ -163,11 +180,8 @@ const MovieDetails = ({ movieId }) => {
 				category="recommendations"
 				movies={recommendations.movies}
 			/>
-			<ScrollableMovieList
-				category="popular"
-				movies={popular.movies}
-			/>
-		</Layout>
+			<ScrollableMovieList category="popular" movies={popular.movies} />
+		</>
 	);
 };
 
