acba5bc5e812670c02b077ceb4db7069f82b0fa5
eka-dyah <eka.dya1205@gmail.com>
2021-05-27 17:24:13 +0700

    add function to search submit button

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,12 +1,26 @@
+import { useRef } from "react";
+import { useHistory } from "react-router-dom";
 import "./SearchBar.css";
 
-const SearchBar = ({ ref, onSubmit }) => (
-	<form onSubmit={onSubmit}>
-		<input name="query" ref={ref}></input>
-		<button>
-			<i className="fa fa-search" aria-hidden="true"></i>
-		</button>
-	</form>
-);
+const SearchBar = () => {
+	const history = useHistory();
+
+	const inputSearchRef = useRef();
+	const onSubmitSearch = (e) => {
+		e.preventDefault();
+		history.push("/search", {
+			keyword: inputSearchRef.current.value
+		})
+		inputSearchRef.current.value = "";
+	}
+	return (
+		<form onSubmit={onSubmitSearch}>
+			<input name="query" ref={inputSearchRef}></input>
+			<button>
+				<i className="fa fa-search" aria-hidden="true"></i>
+			</button>
+		</form>
+	);
+}
 
 export default SearchBar;
